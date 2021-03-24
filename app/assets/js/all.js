dbb68065051bfbc49c20804b81242bc3570ea453
umon752 <umon752@gmail.com>
2021-03-24 09:59:01 +0800

    Update 2021-03-24T01:58:48.546Z

--- app/assets/js/all.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(() => {
-  AOS.init({
-    easing: 'ease',
-    duration: 600,
-    once: true
-  });
-});
---
+++ assets/js/all.js
@@ -0,0 +1,10 @@
+"use strict";
+
+$(document).ready(function () {
+  AOS.init({
+    easing: 'ease',
+    duration: 600,
+    once: true
+  });
+});
+//# sourceMappingURL=all.js.map
